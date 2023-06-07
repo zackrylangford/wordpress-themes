086c05a115a2d1a31ebaf49885ed9dab0ccd2d0e
Zack Langford <zackrylangford.com>
2023-06-07 08:29:08 -0400

    Updated index.php

--- index.php
+++ index.php
@@ -1,11 +1,40 @@
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes(); ?>>
 <head>
-<title> My Theme </title>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
 </head>
-<body>
-<h1> Welcome to My Theme </h1>
+<body <?php body_class(); ?>>
+
+<!-- Header -->
+<header>
+    <h1><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+    <p><?php bloginfo( 'description' ); ?></p>
+</header>
+
+<!-- Main content area -->
+<main>
+    <?php
+    if ( have_posts() ) :
+        while ( have_posts() ) : the_post();
+            the_content();
+        endwhile;
+    else :
+        echo 'No content found';
+    endif;
+    ?>
+</main>
+
+<!-- Footer -->
+<footer>
+    <p>© <?php echo date("Y"); ?> <?php bloginfo( 'name' ); ?></p>
+</footer>
+
+<?php wp_footer(); ?>
+
 </body>
 </html>
+
 
 
